71b741771dbec68aaaf436c84ab03deddf1ef6e8
milendrakumarbaghel <baghelmilendrakumar@gmail.com>
2025-01-11 18:54:32 +0530

    frontend initilised and also created Home page & other routes are defined

---
+++ frontend/src/pages/UserLogin.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const UserLogin = () => {
+  return (
+    <div>
+        <form>
+            <h3>What's your email</h3>
+            <input type='email' placeholder='Email' className='border-b-2 w-full p-2'/>
+        </form>
+    </div>
+  )
+}
+
+export default UserLogin
