6965e5bb35cc08b56b5ebd1b448f0cae9a6d8475
milendrakumarbaghel <baghelmilendrakumar@gmail.com>
2025-02-02 15:17:39 +0530

    created socket context

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -5,14 +5,17 @@ import { BrowserRouter} from 'react-router-dom';
 import { StrictMode } from 'react';
 import UserContext from './context/UserContext.jsx';
 import CaptainContext from './context/CaptainContext.jsx';
+import SocketProvider from './context/SocketContext.jsx';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <CaptainContext>
             <UserContext>
-                <BrowserRouter>
-                    <App />
-                </BrowserRouter>
+                <SocketProvider>
+                    <BrowserRouter>
+                        <App />
+                    </BrowserRouter>
+                </SocketProvider>
             </UserContext>
         </CaptainContext>
     </StrictMode>
