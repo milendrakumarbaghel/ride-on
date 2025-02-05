ec28bd755a6a2fd4913a523da4705f265b42b0e0
milendrakumarbaghel <baghelmilendrakumar@gmail.com>
2025-02-05 12:39:01 +0530

    user and captains details successfully showing on riding and captain riding panel

--- frontend/src/pages/Riding.jsx
+++ frontend/src/pages/Riding.jsx
@@ -23,8 +23,8 @@ const Riding = () => {
                     <img className='h-14' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_538,w_956/v1646663215/assets/6e/e50c1b-2174-4c97-83a1-bfd4544689d0/original/uberX.png" alt="" />
 
                     <div className='text-right'>
-                        <h2 className='text-lg font-medium'>Milendra</h2>
-                        <h4 className='text-xl font-semibold -mt-1 -mb-1'>MP10 AB 1234</h4>
+                        <h2 className='text-lg font-medium'>{ride?.captain?.fullName?.firstName + " " + ride?.captain?.fullName?.lastName}</h2>
+                        <h4 className='text-xl font-semibold -mt-1 -mb-1'>{ride?.captai?.vehicle?.plate}</h4>
                         <p className='text-sm text-gray-600'>Maruti Suzuki Alto</p>
                     </div>
                 </div>
@@ -36,16 +36,16 @@ const Riding = () => {
                             <i className="text-lg ri-map-pin-2-fill"></i>
                             <div>
                                 <h3 className='text-lg font-medium'>562/11-A</h3>
-                                {/* <p className='text-sm -mt-1 text-gray-600'>{props.destination}</p> */}
-                                <p className='text-sm -mt-1 text-gray-600'>aafjd</p>
+                                <p className='text-sm -mt-1 text-gray-600'>{ride?.destination}</p>
+                                {/* <p className='text-sm -mt-1 text-gray-600'>aafjd</p> */}
                             </div>
                         </div>
                         <div className='flex items-center gap-5 p-3'>
                             <i className="ri-currency-line"></i>
                             <div>
-                                {/* <h3 className='text-lg font-medium'>₹{props.fare[ props.vehicleType ]} </h3> */}
-                                <h3 className='text-lg font-medium'>₹ Paisa </h3>
-                                <p className='text-sm -mt-1 text-gray-600'>Cash Cash</p>
+                                <h3 className='text-lg font-medium'>₹{ride?.fare} </h3>
+                                {/* <h3 className='text-lg font-medium'>₹ Paisa </h3> */}
+                                <p className='text-sm -mt-1 text-gray-600'>Cash</p>
                             </div>
                         </div>
                     </div>
