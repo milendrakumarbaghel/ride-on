198b71174c2765cf455b4cb2890820495ad356f5
milendrakumarbaghel <baghelmilendrakumar@gmail.com>
2025-02-06 11:50:59 +0530

    solving error

--- frontend/src/pages/Riding.jsx
+++ frontend/src/pages/Riding.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link, useLocation } from 'react-router-dom'
-import { useState, useEffect, useContext } from 'react'
+import { useContext } from 'react'
 import { SocketContext } from '../context/SocketContext';
 import { useNavigate } from 'react-router-dom'
 
@@ -18,24 +18,20 @@ const Riding = () => {
 
     return (
         <div className='h-screen'>
-
-            <Link to='/home' className='fixed h10 w-10 right-2 top-2 bg-white flex items-center justify-center rounded-full'>
-                <i className="text-lg font-medium ri-home-4-line"></i>
+            <Link to='/home' className='fixed right-2 top-2 h-10 w-10 bg-white flex items-center justify-center rounded-full'>
+                <i className="text-lg font-medium ri-home-5-line"></i>
             </Link>
-
             <div className='h-1/2'>
                 <img className='h-full w-full object-cover' src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif" alt="" />
             </div>
-
             <div className='h-1/2 p-4'>
-
                 <div className='flex items-center justify-between'>
-                    <img className='h-14' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_538,w_956/v1646663215/assets/6e/e50c1b-2174-4c97-83a1-bfd4544689d0/original/uberX.png" alt="" />
-
+                    <img className='h-12' src="https://swyft.pl/wp-content/uploads/2023/05/how-many-people-can-a-uberx-take.jpg" alt="" />
                     <div className='text-right'>
                         <h2 className='text-lg font-medium capitalize'>{ride?.captain?.fullName?.firstName + " " + ride?.captain?.fullName?.lastName}</h2>
                         <h4 className='text-xl font-semibold -mt-1 -mb-1'>{ride?.captain?.vehicle?.plate}</h4>
                         <p className='text-sm text-gray-600'>Maruti Suzuki Alto</p>
+
                     </div>
                 </div>
 
@@ -47,26 +43,19 @@ const Riding = () => {
                             <div>
                                 <h3 className='text-lg font-medium'>562/11-A</h3>
                                 <p className='text-sm -mt-1 text-gray-600'>{ride?.destination}</p>
-                                {/* <p className='text-sm -mt-1 text-gray-600'>aafjd</p> */}
                             </div>
                         </div>
                         <div className='flex items-center gap-5 p-3'>
                             <i className="ri-currency-line"></i>
                             <div>
                                 <h3 className='text-lg font-medium'>₹{ride?.fare} </h3>
-                                {/* <h3 className='text-lg font-medium'>₹ Paisa </h3> */}
-                                <p className='text-sm -mt-1 text-gray-600'>Cash</p>
+                                <p className='text-sm -mt-1 text-gray-600'>Cash Cash</p>
                             </div>
                         </div>
                     </div>
                 </div>
-                <button
-                    className='w-full mt-5 bg-green-600 text-white font-semibold p-2 rounded-lg'
-                >
-                    Make a Payment
-                </button>
+                <button className='w-full mt-5 bg-green-600 text-white font-semibold p-2 rounded-lg'>Make a Payment</button>
             </div>
-
         </div>
     )
 }
