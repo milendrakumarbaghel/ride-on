4e2225dafc234d6206e42a9050cf361c7d30832f
milendrakumarbaghel <baghelmilendrakumar@gmail.com>
2025-09-12 17:51:00 +0530

    CI/CD Check 1

--- backend/server.js
+++ backend/server.js
@@ -43,7 +43,7 @@ const connectToDatabase = require('./db/database');
 connectToDatabase();
 
 initializeSocket(server);
-//CI/CD Check
+//CI/CD Check 1
 // Start the server
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
